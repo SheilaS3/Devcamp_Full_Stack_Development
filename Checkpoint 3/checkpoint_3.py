80966b8aa17ed1e503dd627172facb604ed321cc
SheilaS3 <151578276+SheilaS3@users.noreply.github.com>
2024-03-20 23:13:10 +0100

    Add files via upload

---
+++ Checkpoint 3/checkpoint_3.py
@@ -0,0 +1,39 @@
+# Exercise 1:
+string = "It's almost summer!"
+number = 5
+list = ["spring", "summer", "fall", "winter"]
+boolean = True
+
+# Exercise 2: not counting the apostrophe as a letter
+modified_string = string[:4]
+
+# Exercise 3:
+list_first_element = list[0]
+
+# Exercise 4:
+modified_number = number + 10 
+
+# Exercise 5:
+list_last_element = list[3]
+
+# Exercise 6:
+names = 'harry,alex,susie,jared,gail,conner'
+
+list_of_names = names.split(',')
+
+# Exercise 7:
+upper_string = string[:4].upper()
+
+new_string = string.replace('It\'s', upper_string)
+# Exercise 8:
+sentence = f"I\'m ready to go on a trip in {number} days!"
+
+# Exercise 9:
+print('hello world')
+
+# Exercise 10:
+cadena = "Hola, encantada de conocerte."
+
+buscar_hola = cadena.find('Hola')
+
+cadena = cadena.replace('Hola', 'Adios')
